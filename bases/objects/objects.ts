0cd1e8612d72066c8a01ed7b8df5ac8a125ed809
Sergio <sergiorodasgonzalez@gmail.com>
2023-07-18 14:14:37 -0300

    Add objects and custom types

---
+++ bases/objects/objects.ts
@@ -0,0 +1,15 @@
+(() => {
+    let flash: {name: string, age?: number, powers:number[], getName?: () => string } = {
+        name: 'Barry Alen',
+        age: 24,
+        powers: [1, 2],
+    }
+
+    let superman: {name: string, age?: number, powers:number[], getName?: () => string } = {
+        name: 'Clark Kent',
+        age: 60,
+        powers: [1],
+    }
+
+    // console.log(flash.getName());
+})();
